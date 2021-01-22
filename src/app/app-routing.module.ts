c283f5514f24c942ce9c7cb205875b04c50ae2e2
truizdiaz <tomiruizdiaz@gmail.com>
2021-01-22 10:11:51 -0300

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 
-const routes: Routes = [];
+// componentes
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+
+const routes: Routes = [
+  { path: '', component: ListarProductosComponent },
+  { path: 'crear-producto', component: CrearProductoComponent },
+  { path: 'editar-producto/:id', component: CrearProductoComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
