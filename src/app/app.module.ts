8a728ce204b4fbf22d267c0cb51cd48950faa6c1
truizdiaz <tomiruizdiaz@gmail.com>
2021-01-22 11:07:18 -0300

    Crear Producto - Formulario Reactivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
@@ -14,7 +17,8 @@ import { ListarProductosComponent } from './components/listar-productos/listar-p
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-producto',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-producto.component.css']
 })
 export class CrearProductoComponent implements OnInit {
+  productoForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { 
+    this.productoForm = this.fb.group({
+      producto: ['', Validators.required],
+      categoria: ['', Validators.required],
+      ubicacion: ['', Validators.required],
+      precio: ['', Validators.required],
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  agregarProducto() {
+    console.log(this.productoForm)
   }
 
 }
